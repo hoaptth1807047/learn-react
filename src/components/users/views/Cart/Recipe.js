cbedd4a37d14eb08ef70bcd4ae0b41fc7dabd936
thanh hoa <hoaptth1807047@fpt.edu.vn>
2019-10-31 11:11:24 +0700

    thanh toan

--- src/components/users/views/Cart/Recipe.js
+++ src/components/users/views/Cart/Recipe.js
@@ -1,21 +1,8 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Link} from "react-router-dom";
 
-//import { addShipping } from './actions/cartActions'
 class Recipe extends Component {
-
-    // componentWillUnmount() {
-    //     if (this.refs.shipping.checked)
-    //         this.props.substractShipping()
-    // }
-
-    handleChecked = (e) => {
-        if (e.target.checked) {
-            this.props.addShipping();
-        } else {
-            this.props.substractShipping();
-        }
-    };
 
     render() {
         return (
@@ -24,42 +11,8 @@ class Recipe extends Component {
                     <div>Total : {this.props.total}</div>
                 </div>
                 <button type="button" className="btn btn success">
-                    Checkout
+                    <Link to="/pay">Checkout</Link>
                 </button>
-                {/*<tr>*/}
-                {/*    <td style={textDetail} onChange={this.handleChecked}>*/}
-                {/*        Estimated shipping*/}
-                {/*    </td>*/}
-                {/*    <td/>*/}
-                {/*    <td/>*/}
-                {/*    <td/>*/}
-                {/*    <td style={textDetail}>Shipping(+6$)</td>*/}
-                {/*</tr>*/}
-                {/*<tr>*/}
-                {/*    <td style={textDetail}>*/}
-                {/*        Total*/}
-                {/*    </td>*/}
-                {/*    <td/>*/}
-                {/*    <td/>*/}
-                {/*    <td/>*/}
-                {/*    <td style={textDetail}>${this.props.total}</td>*/}
-                {/*</tr>*/}
-                {/*<tr>*/}
-                {/*    <th>*/}
-                {/*        <button type="button" className="btn btn-default">*/}
-                {/*            <span className="glyphicon glyphicon-shopping-cart"/> Continue*/}
-                {/*            Shopping*/}
-                {/*        </button>*/}
-                {/*    </th>*/}
-                {/*    <td/>*/}
-                {/*    <td/>*/}
-                {/*    <td/>*/}
-                {/*    <td>*/}
-                {/*        <button type="button" className="btn btn-success">*/}
-                {/*            Checkout <span className="glyphicon glyphicon-play"/>*/}
-                {/*        </button>*/}
-                {/*    </td>*/}
-                {/*</tr>*/}
             </div>
         )
     }
